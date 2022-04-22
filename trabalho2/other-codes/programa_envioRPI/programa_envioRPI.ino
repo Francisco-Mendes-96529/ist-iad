98a8c5a53bcf09499b18d902cf57b790ed43d476
marianaribeiro13 <80052786+marianaribeiro13@users.noreply.github.com>
2022-04-22 13:11:32 +0100

    updated Arduino

--- trabalho2/other-codes/programa_envioRPI/programa_envioRPI.ino
+++ trabalho2/other-codes/programa_envioRPI/programa_envioRPI.ino
@@ -1,25 +1,60 @@
 struct Programa{
   int ativo=0; //0-false, 1-true
-  int d[7]={0,0,0,0,0,0,0}; //dia
+  int d[7]={0,0,0,0,0,0,0}; //dia 0-false 2-true
   int hi=0, mi=0; //hora, minutos iniciais
   int hf=0, mf=0; //hora, minutos finais
-  int canais[12]={0,0,0,0,0,0,0,0,0,0,0,0};
+  int canais[12]={0,0,0,0,0,0,0,0,0,0,0,0}; // 0-false 2-true
   int fonte=0; //torneira-0, bomba-1
-  int sensor[2]={0,0};
+  int sensor[2]={0,0}; // 0-false 2-true
 }prog[20];
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Arduino is ready");
+  prog[0].ativo=1;
+  prog[0].hi=12;
+  prog[0].mi=30;
+  prog[4].ativo=1;
+  prog[4].d[1]=2;
+  prog[4].d[2]=2;
+  prog[4].hi=12;
+  prog[4].mi=30;
+  prog[4].canais[1]=2; 
+  prog[4].hf=12;
+  prog[4].mf=45;
+  prog[4].fonte=1;
+  prog[4].sensor[0]=2;
 }
 
 void loop() {
   if(Serial.available()>0){
    int inByte = Serial.read();
    if (inByte=='e') enviar();
+   if (inByte=='r') receber();
   }
 }
 
+void receber(){
+  int k = int(Serial.read());
+  prog[k].ativo = int(Serial.read());
+  for (int j=0; j<7; j++)
+    {
+      prog[k].d[j]=int(Serial.read());
+    }
+  prog[k].hi = int(Serial.read());
+  prog[k].mi = int(Serial.read());
+  prog[k].hf = int(Serial.read());
+  prog[k].mf = int(Serial.read());
+  for (int j=0; j<12; j++)
+    {
+      prog[k].canais[j]=int(Serial.read());
+    }  
+  prog[k].fonte = int(Serial.read());
+  for (int j=0; j<2; j++)
+    {
+      prog[k].sensor[j]=int(Serial.read());
+    }
+}
 void enviar(){
   for (int i=0; i<20; i++)
   {
@@ -49,5 +84,5 @@ void enviar(){
     Serial.flush();
     Serial.println(prog[i].sensor[1]);
     Serial.flush();
-  }
+  }  
 }
