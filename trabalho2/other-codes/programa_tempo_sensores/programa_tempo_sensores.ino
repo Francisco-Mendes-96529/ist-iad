7017343a224a6270fe2d5428ccf4254116f11e64
marianaribeiro13 <80052786+marianaribeiro13@users.noreply.github.com>
2022-04-28 23:35:05 +0100

    updated final arduino

--- trabalho2/other-codes/programa_tempo_sensores/programa_tempo_sensores.ino
+++ trabalho2/other-codes/programa_tempo_sensores/programa_tempo_sensores.ino
@@ -1,14 +1,12 @@
-
-
 struct Programa{
   int ativo=0; //0-false, 1-true
-  int d[7]={0,0,0,0,0,0,0}; //dia 0-false 2-true
+  int d[7]={0,0,0,0,0,0,0}; //dia 0-false 1-true
   int hi=0, mi=0; //hora, minutos iniciais
   int hf=0, mf=0; //hora, minutos finais
-  int canais[12]={0,0,0,0,0,0,0,0,0,0,0,0}; // 0-false 2-true
+  int canais[12]={0,0,0,0,0,0,0,0,0,0,0,0}; // 0-false 1-true
   int fonte=0; //torneira-0, bomba-1
-  int sensor[2]={0,0}; // 0-false 2-true
-}prog[20];
+  int sensor[2]={0,0}; // 0-false 1-true
+} prog[20];
 
 int dia, hora, mins, seg;
 unsigned long start_time=0, curr_time=0, ant_time=0;
@@ -23,19 +21,6 @@ bool al5 = !al3;
 void setup() {
   Serial.begin(9600);
   Serial.println("Arduino is ready");
-  prog[0].ativo=1;
-  prog[0].hi=12;
-  prog[0].mi=30;
-  prog[4].ativo=1;
-  prog[4].d[1]=2;
-  prog[4].d[2]=2;
-  prog[4].hi=12;
-  prog[4].mi=30;
-  prog[4].canais[1]=2; 
-  prog[4].hf=12;
-  prog[4].mf=45;
-  prog[4].fonte=1;
-  prog[4].sensor[0]=2;
   for(int i = 0; i<12; i++){
     pinMode(canal_pin[i][0], OUTPUT);  
   }
@@ -50,13 +35,13 @@ void setup() {
 void loop() {
   if(Serial.available()>0){
    int inByte = Serial.read();
-   if (inByte=='t') tempo();
-   if (inByte=='T') enviar_tempo();
+   if (inByte=='t') tempo(); 
+   if (inByte=='T') enviar_tempo(); //debug
    if (inByte=='e') enviar();
    if (inByte=='r') receber();
-   if (inByte=='k') enviar_k();
+   if (inByte=='k') enviar_k(); //debug
    if (inByte=='a') receber_ativo(); 
-   //if (inByte=='i') 
+   if (inByte=='s') leitura_sensores(); //debug
   }
   tempo_atual();
   for (int i=0; i<20; i++){
@@ -67,62 +52,81 @@ void loop() {
       int hf =prog[i].hf;
       if(prog[i].d[dia_atual]==1 && ((hi==hf && hora_atual==hi && min_atual>=mi && min_atual<mf) || (hi!=hf && ((hora_atual>hi && hora_atual<hf) || (hora_atual==hi && min_atual>=mi) || (hora_atual==hf && min_atual<mf)))))
       {
-         digitalWrite(A2, !prog[i].fonte);
-         digitalWrite(A3, prog[i].fonte);
-         for(int k=0; k<2; i++){
-          if (prog[i].sensor[k]==1){
-            nivel_humidade[k] = analogRead(sensor_pin[k]);
-            if(nivel_humidade[k] >= 377*al3 + 749*al5 && nivel_humidade[k] <= 437*al3 + 807*al5){
-              Serial.println("Ar");
-              }
-            else if(nivel_humidade[k] > 327*al3 + 726*al5 && nivel_humidade[k] <= 377*al3 + 749*al5){
-              Serial.println("Solo seco");
-            }
-            else if(nivel_humidade[k] > 285*al3 + 629*al5 && nivel_humidade[k] <= 327*al3 + 726*al5){
-             Serial.println("Solo semi húmido");
-            }
-            else if(nivel_humidade[k] > 258*al3 + 535*al5 && nivel_humidade[k] <= 285*al3 + 629*al5){
-              Serial.println("Solo húmido");
-            }
-            else if(nivel_humidade[k] > 223*al3 + 473*al5 && nivel_humidade[k] <= 258*al3 + 535*al5){
-              Serial.println("Água");
-            }
-            else{
-              Serial.println("Valor lido inválido. Verifique a qualidade do sensor.");
-            }
-          }  
-  }
-         for(int k = 0; k<12; k++){
-          if(prog[i].canais[k]==1) {
-            canal_pin[k][1]=i;
-            digitalWrite(canal_pin[k][0], 1);
+        if(prog[i].sensor[0]==1 || prog[i].sensor[1]==1){
+          nivel_humidade[0] = analogRead(sensor_pin[0])*prog[i].sensor[0]; //se estiver desligado, entao dá 0, nao contribui para a decisao de regar
+          nivel_humidade[1] = analogRead(sensor_pin[1])*prog[i].sensor[1];
+          if(nivel_humidade[0] > 285*al3 + 629*al5 || nivel_humidade[1] > 285*al3 + 629*al5){
+            regar(i);
           }
-        
+            else{
+              //parar de regar se ambos estiverem muito humidos
+              parar_regar(i);
+            }
+        }
+        else{
+          //se ambos os sensores estiverem desligados, rega
+          regar(i);
         }
       }
       else{
-        /*digitalWrite(A2, 0);
-        digitalWrite(A3, 0);*/
-        for(int k = 0; k<12; k++){
-          if(canal_pin[k][1]==i){
-            canal_pin[k][1]=-1;
-            digitalWrite(canal_pin[k][0], 0);
-          }
-        }
+        //se nao estiver na hora de rega, para de regar
+        parar_regar(i);
       }
     }
     else{
-      for(int k = 0; k<12; k++){
-          if(canal_pin[k][1]==i){
-            canal_pin[k][1]=-1;
-            digitalWrite(canal_pin[k][0], 0);
-          }
-      }
+      //se nao estiver ativo
+      parar_regar(i);
     }
   }
-   
-  digitalWrite(A2, !prog[0].fonte);
-  digitalWrite(A3, prog[0].fonte);
+}
+
+void regar(int i){ 
+  digitalWrite(A2, !prog[i].fonte);
+   digitalWrite(A3, prog[i].fonte);
+   for(int j = 0; j<12; j++){
+      if(prog[i].canais[j]==1) {
+        canal_pin[j][1]=i;
+        digitalWrite(canal_pin[j][0], 1);
+      }
+   }
+}
+
+void parar_regar(int i){
+  for(int k = 0; k<12; k++){
+    if(canal_pin[k][1]==i){
+      canal_pin[k][1]=-1;
+      digitalWrite(canal_pin[k][0], 0);
+    }
+  }
+}
+
+void leitura_sensores(){
+  for(int k=0; k<2; k++){
+    nivel_humidade[k] = analogRead(sensor_pin[k]);
+    if(nivel_humidade[k] >= 377*al3 + 749*al5 && nivel_humidade[k] <= 437*al3 + 807*al5){
+      String temp= "Ar: " + String(nivel_humidade[k]);
+      Serial.println(temp);
+      }
+    else if(nivel_humidade[k] > 327*al3 + 726*al5 && nivel_humidade[k] <= 377*al3 + 749*al5){
+      String temp= "Solo seco: " + String(nivel_humidade[k]);
+      Serial.println(temp);
+    }
+    else if(nivel_humidade[k] > 285*al3 + 629*al5 && nivel_humidade[k] <= 327*al3 + 726*al5){
+      String temp= "Solo meio húmido: " + String(nivel_humidade[k]);
+      Serial.println(temp);
+    }
+    else if(nivel_humidade[k] > 258*al3 + 535*al5 && nivel_humidade[k] <= 285*al3 + 629*al5){
+      String temp= "Solo húmido: " + String(nivel_humidade[k]);
+      Serial.println(temp);
+    }
+    else if(nivel_humidade[k] > 223*al3 + 473*al5 && nivel_humidade[k] <= 258*al3 + 535*al5){
+      String temp= "Água: " + String(nivel_humidade[k]);
+      Serial.println(temp);
+    }
+    else{
+      Serial.println("Valor lido inválido. Verifique a qualidade do sensor ou da ligação :):");
+    }
+  }
 }
 
 void tempo() {
@@ -146,9 +150,6 @@ void tempo() {
   }
 }
 
- unsigned long converter_tempo(int d, int h, int m ,int s) {
-return d*86400+m*60+s+h*3600UL;
-}
 void tempo_atual(){
   curr_time=millis()/1000;
   unsigned long diff_times=curr_time-ant_time;
@@ -169,25 +170,17 @@ void tempo_atual(){
   ant_time = curr_time;
 }
 
-void enviar_tempo(){
-  //tempo_atual();
-  /*unsigned long temp_seg_atual = seg_atual;
-  unsigned long temp_dia=temp_seg_atual/(86400);
-  temp_seg_atual -= temp_dia*24*3600;
-  unsigned long temp_hora=temp_seg_atual/3600;
-  temp_seg_atual -= temp_hora*(3600);
-  unsigned long temp_min=temp_seg_atual/60;
-  unsigned long temp_seg= temp_seg_atual-temp_min*60;*/
+void enviar_tempo() // função de debug do tempo enviado pelo Raspberry PI inicialmente e do tempo atual
+{
   String strtempo =  " -- Dia: " + String(dia) + " -- " + String(hora) + ":" + String(mins) + ":" + String(seg) + " -- Dia: " + String(dia_atual) + " -- " + String(hora_atual) + ":" + String(min_atual) + ":" + String(seg_atual);
   Serial.println(strtempo);
   Serial.flush();
 }
+
 void receber(){
   Serial.println("recebendo...");
   Serial.flush();
   int k = Serial.readStringUntil('\n').toInt();
-
-
   prog[k].ativo = Serial.readStringUntil('\n').toInt();
   for (int j=0; j<7; j++)
     {
